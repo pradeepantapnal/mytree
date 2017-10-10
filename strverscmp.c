e95006fa843523872c41d9cd2b580b2f89bb34a7
Pradeep Antapnal <antapnp1@nx-2x5l622.pace.internal>
2017-10-10 11:29:06 +0530

    format changes

--- strverscmp.c
+++ strverscmp.c
@@ -22,7 +22,7 @@
 /* #include "safe-ctype.h" */
 #include <ctype.h>
 
-/* 
+/*
 @deftypefun int strverscmp (const char *@var{s1}, const char *@var{s2})
 The @code{strverscmp} function compares the string @var{s1} against
 @var{s2}, considering them as holding indices/version numbers.  Return
@@ -69,7 +69,7 @@ This function is especially useful when dealing with filename sorting,
 because filenames frequently hold indices/version numbers.
 @end deftypefun
 
-*/
+ */
 
 /* states: S_N: normal, S_I: comparing integral part, S_F: comparing
            fractional parts, S_Z: idem but with leading Zeroes only */
@@ -82,7 +82,6 @@ because filenames frequently hold indices/version numbers.
 #define  CMP    2
 #define  LEN    3
 
-
 /* Compare S1 and S2 as strings holding indices/version numbers,
    returning less than, equal to or greater than zero if S1 is less than,
    equal to or greater than S2 (for more info, see the Glibc texinfo doc).  */
@@ -90,69 +89,69 @@ because filenames frequently hold indices/version numbers.
 int
 strverscmp (const char *s1, const char *s2)
 {
-  const unsigned char *p1 = (const unsigned char *) s1;
-  const unsigned char *p2 = (const unsigned char *) s2;
-  unsigned char c1, c2;
-  int state;
-  int diff;
+	const unsigned char *p1 = (const unsigned char *) s1;
+	const unsigned char *p2 = (const unsigned char *) s2;
+	unsigned char c1, c2;
+	int state;
+	int diff;
 
-  /* Symbol(s)    0       [1-9]   others  (padding)
+	/* Symbol(s)    0       [1-9]   others  (padding)
      Transition   (10) 0  (01) d  (00) x  (11) -   */
-  static const unsigned int next_state[] =
-    {
-      /* state    x    d    0    - */
-      /* S_N */  S_N, S_I, S_Z, S_N,
-      /* S_I */  S_N, S_I, S_I, S_I,
-      /* S_F */  S_N, S_F, S_F, S_F,
-      /* S_Z */  S_N, S_F, S_Z, S_Z
-    };
+	static const unsigned int next_state[] =
+	{
+			/* state    x    d    0    - */
+			/* S_N */  S_N, S_I, S_Z, S_N,
+			/* S_I */  S_N, S_I, S_I, S_I,
+			/* S_F */  S_N, S_F, S_F, S_F,
+			/* S_Z */  S_N, S_F, S_Z, S_Z
+	};
 
-  static const int result_type[] =
-    {
-      /* state   x/x  x/d  x/0  x/-  d/x  d/d  d/0  d/-
+	static const int result_type[] =
+	{
+			/* state   x/x  x/d  x/0  x/-  d/x  d/d  d/0  d/-
                  0/x  0/d  0/0  0/-  -/x  -/d  -/0  -/- */
 
-      /* S_N */  CMP, CMP, CMP, CMP, CMP, LEN, CMP, CMP,
-                 CMP, CMP, CMP, CMP, CMP, CMP, CMP, CMP,
-      /* S_I */  CMP, -1,  -1,  CMP, +1,  LEN, LEN, CMP,
-                 +1,  LEN, LEN, CMP, CMP, CMP, CMP, CMP,
-      /* S_F */  CMP, CMP, CMP, CMP, CMP, LEN, CMP, CMP,
-                 CMP, CMP, CMP, CMP, CMP, CMP, CMP, CMP,
-      /* S_Z */  CMP, +1,  +1,  CMP, -1,  CMP, CMP, CMP,
-                 -1,  CMP, CMP, CMP
-    };
+			/* S_N */  CMP, CMP, CMP, CMP, CMP, LEN, CMP, CMP,
+			CMP, CMP, CMP, CMP, CMP, CMP, CMP, CMP,
+			/* S_I */  CMP, -1,  -1,  CMP, +1,  LEN, LEN, CMP,
+			+1,  LEN, LEN, CMP, CMP, CMP, CMP, CMP,
+			/* S_F */  CMP, CMP, CMP, CMP, CMP, LEN, CMP, CMP,
+			CMP, CMP, CMP, CMP, CMP, CMP, CMP, CMP,
+			/* S_Z */  CMP, +1,  +1,  CMP, -1,  CMP, CMP, CMP,
+			-1,  CMP, CMP, CMP
+	};
 
-  if (p1 == p2)
-    return 0;
+	if (p1 == p2)
+		return 0;
 
-  c1 = *p1++;
-  c2 = *p2++;
-  /* Hint: '0' is a digit too.  */
-  state = S_N | ((c1 == '0') + (isdigit (c1) != 0));
+	c1 = *p1++;
+	c2 = *p2++;
+	/* Hint: '0' is a digit too.  */
+	state = S_N | ((c1 == '0') + (isdigit (c1) != 0));
 
-  while ((diff = c1 - c2) == 0 && c1 != '\0')
-    {
-      state = next_state[state];
-      c1 = *p1++;
-      c2 = *p2++;
-      state |= (c1 == '0') + (isdigit (c1) != 0);
-    }
+	while ((diff = c1 - c2) == 0 && c1 != '\0')
+	{
+		state = next_state[state];
+		c1 = *p1++;
+		c2 = *p2++;
+		state |= (c1 == '0') + (isdigit (c1) != 0);
+	}
 
-  state = result_type[state << 2 | (((c2 == '0') + (isdigit (c2) != 0)))];
+	state = result_type[state << 2 | (((c2 == '0') + (isdigit (c2) != 0)))];
 
-  switch (state)
-    {
-    case CMP:
-      return diff;
-      
-    case LEN:
-      while (isdigit (*p1++))
-	if (!isdigit (*p2++))
-	  return 1;
-      
-      return isdigit (*p2) ? -1 : diff;
-      
-    default:
-      return state;
-    }
+	switch (state)
+	{
+	case CMP:
+		return diff;
+
+	case LEN:
+		while (isdigit (*p1++))
+			if (!isdigit (*p2++))
+				return 1;
+
+		return isdigit (*p2) ? -1 : diff;
+
+	default:
+		return state;
+	}
 }
